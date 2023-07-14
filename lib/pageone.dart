f2c1785f6cd3db5af663f66d531f2bf5870d1e0e
Dr. Raed <drraed.jb@gmail.com>
2023-07-15 00:15:31 +0200

    example of BottomNavigationBar

---
+++ lib/pageone.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PageOne extends StatefulWidget {
+  const PageOne({super.key});
+
+  @override
+  State<PageOne> createState() => _PageOneState();
+}
+
+class _PageOneState extends State<PageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("example of BottomNavigationBar "),
+      ),
+    );
+  }
+}
